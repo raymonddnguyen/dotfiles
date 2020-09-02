2d8ceba31219609d6617e448030e83faa76a1602
Raymond Nguyen <raymonddnguyen@gmail.com>
2020-09-01 22:58:54 -0500

    Changed polybar to work both for mobile and docked laptop

--- polybar/.config/polybar/themes/blocks/launch.sh
+++ polybar/.config/polybar/themes/blocks/launch.sh
@@ -6,10 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# polybar -rq dummy & 
+# polybar -rq dummy &
 # polybar -rq music &
 polybar tray -c ~/.config/polybar/themes/blocks/config &
 polybar misc -c ~/.config/polybar/themes/blocks/config &
 polybar bspwm -c ~/.config/polybar/themes/blocks/config &
+
+# 2nd monitor for external
+# dp_1=$(eval "xrandr --query | rg 'DP-1 connected 2560'")
+# echo "$dp_1"
+thunderbolt_dir=/sys/devices/pci0000:00/0000:00:1d.4/0000:03:00.0/0000:04:00.0/0000:05:00.0/domain0/0-0/0-1
+# if [[ $dp_1 == *"connected"* ]]; then
+if [[ -d $thunderbolt_dir ]]; then
+    polybar tray2 -c ~/.config/polybar/themes/blocks/config &
+    polybar misc2 -c ~/.config/polybar/themes/blocks/config &
+    polybar bspwm2 -c ~/.config/polybar/themes/blocks/config &
+fi
 
 echo "Polybar launched..."
