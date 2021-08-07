468a9c3efc5c1f262ac802034b80adc35d02ff5b
Raymond Nguyen <raymonddnguyen@gmail.com>
2021-08-07 15:49:47 -0500

    Set up polybar for 1 laptop monitor, 1 horizontal and 1 vertical

--- polybar/.config/polybar/themes/blocks/launch.sh
+++ polybar/.config/polybar/themes/blocks/launch.sh
@@ -18,9 +18,18 @@ polybar bspwm -c ~/.config/polybar/themes/blocks/config &
 thunderbolt_dir=/sys/devices/pci0000:00/0000:00:1d.4/0000:03:00.0/0000:04:00.0/0000:05:00.0/domain0/0-0/0-1
 # if [[ $dp_1 == *"connected"* ]]; then
 if [[ -d $thunderbolt_dir ]]; then
-    polybar tray2 -c ~/.config/polybar/themes/blocks/config &
-    polybar misc2 -c ~/.config/polybar/themes/blocks/config &
-    polybar bspwm2 -c ~/.config/polybar/themes/blocks/config &
+    if xrandr -q | rg -i 'dp-2 connected 1440' &>/dev/null; then
+        polybar trayvertical -c ~/.config/polybar/themes/blocks/config &
+        polybar bspwmvertical -c ~/.config/polybar/themes/blocks/config &
+
+        polybar misclaptop -c ~/.config/polybar/themes/blocks/config &
+        polybar traylaptop -c ~/.config/polybar/themes/blocks/config &
+        polybar bspwmlaptop -c ~/.config/polybar/themes/blocks/config &
+    else
+        polybar misc2 -c ~/.config/polybar/themes/blocks/config &
+        polybar tray2 -c ~/.config/polybar/themes/blocks/config &
+        polybar bspwm2 -c ~/.config/polybar/themes/blocks/config &
+    fi
 fi
 
 echo "Polybar launched..."
